f57298b17426296e4a1e1dd8b73dad979d55dac1
陈煜坚 <chenyujian@oigcn.cn>
2023-08-01 10:24:33 +0800

    📃 docs: 完善《Vercel 关联个人域名》

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import VueMacros from 'unplugin-vue-macros/vite'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  server: {
-    open: false
-  },
+  // server: {
+  //   open: false
+  // },
   plugins: [VueMacros(), UnoCSS()]
 })
